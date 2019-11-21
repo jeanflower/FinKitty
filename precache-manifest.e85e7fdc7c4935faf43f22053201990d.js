5e6bde136198d288f809063a2665c030930b96f9
Jean Flower <jeanflower@rocketmail.com>
2019-11-21 09:53:09 +0000

    Updates

---
+++ precache-manifest.e85e7fdc7c4935faf43f22053201990d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38af262042aa2bb292600742c11b1da0",
+    "url": "/FinKitty/index.html"
+  },
+  {
+    "revision": "7c1938abb9326a883d32",
+    "url": "/FinKitty/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "554d65951146c3939108",
+    "url": "/FinKitty/static/js/2.d352b96f.chunk.js"
+  },
+  {
+    "revision": "7c1938abb9326a883d32",
+    "url": "/FinKitty/static/js/main.1a4efa99.chunk.js"
+  },
+  {
+    "revision": "8c8e83f48899812720a0",
+    "url": "/FinKitty/static/js/runtime-main.12432f88.js"
+  }
+]);
