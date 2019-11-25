286c4ffcbf59bd078dae746afad93c0fba4440fd
Jean Flower <jeanflower@rocketmail.com>
2019-11-25 22:43:40 +0000

    Linting fixes

--- src/reactComponents/Input.jsx
+++ src/reactComponents/Input.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Input = props => {
   //log(props.value);
   return (
-    <div className='form-group'>
-      <label htmlFor={props.name} className='form-label'>
+    <div className="form-group">
+      <label htmlFor={props.name} className="form-label">
         {props.title}
       </label>
       <input
-        className='form-control'
+        className="form-control"
         id={props.name}
         name={props.name}
         type={props.type}
