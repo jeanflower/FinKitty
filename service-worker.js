efb9284e98aaf0facc1ec7919d5effbad152614d
Jean Flower <jeanflower@rocketmail.com>
2021-11-21 16:05:22 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FinKitty/precache-manifest.0c7c933da85826dfa055554bde2e82fc.js"
+  "/FinKitty/precache-manifest.6523c5627d684dbbd627d4ce2e5d77fa.js"
 );
 
 self.addEventListener('message', (event) => {
