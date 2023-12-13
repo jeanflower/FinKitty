7d5c86846ba9a01cdc59056e55b2e24e06d5a088
Jean Flower <jeanflower@rocketmail.com>
2023-12-13 11:24:21 +0000

    Table font size

--- views/reactComponents/DataGridFinKitty.tsx
+++ views/reactComponents/DataGridFinKitty.tsx
@@ -292,7 +292,8 @@ class DataGridFinKitty extends React.Component<DataGridProps, DataGridState> {
         {this.props.rows && (
           <ReactDataGrid
             style={{
-              height: this.props.rows.length * 35 + 40,
+              height: this.props.rows.length * 35 + 60,
+              fontSize: 20,
             }}
             columns={this.props.columns}
             onRowsChange={(rows: any[], data: RowsChangeData<any, any>) => {
