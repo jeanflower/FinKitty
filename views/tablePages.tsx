83a80a94426fc1a54d52f9aa57e2f43242be43b5
Jean Flower <jeanflower@rocketmail.com>
2023-12-15 20:58:16 +0000

    Use auto width for date column in Dates table

--- views/tablePages.tsx
+++ views/tablePages.tsx
@@ -1951,6 +1951,7 @@ function triggersTableDiv(
               },
               {
                 ...triggerDateColumn(model),
+                width: 'auto',
                 key: "DATE",
                 name: "date",
               },
